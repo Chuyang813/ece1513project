cc6b74e326b4d17582941b89ae49ad2926faa30c
Chuyang Li <19cl98@queensu.ca>
2025-03-27 23:05:50 -0400

    Create movieBaseline.py

---
+++ movieBaseline.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def read_file(file_path):
+    ratings = pd.read_csv(
+        file_path,
+        sep='::',
+        names=['user_id', 'movie_id', 'rating', 'timestamp'],
+        engine='python'
+    )
+
+
+
+if __name__ == '__main__':
+    
+    ratings = read_file('ml-1m/ratings.dat')
+    print(ratings.head())
